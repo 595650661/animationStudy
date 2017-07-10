f57d892e212995d62fa7fc585e8142fe588d49c1
jiangjunhui <2806139037@qq.com>
2017-07-10 11:40:48 +0800

    添加基础动画

---
+++ PopAnimation/PopAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PopAnimation
+//
+//  Created by jiangjunhui on 2017/7/10.
+//  Copyright © 2017年 jiangjunhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
